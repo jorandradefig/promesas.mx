61cc1e8d6c9214a7e018407698ba6c208c136a00
Jorge Andrade <jorandradefig@gmail.com>
2017-09-22 19:23:17 -0500

    feature(home.component): component layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
     NavbarComponent
   ],
   imports: [
